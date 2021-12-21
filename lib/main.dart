4484daa067da5df95471606e4d103915842d1de6
rmartey <marteyrichmond001@gmail.com>
2021-12-21 22:55:00 +0000

    developed UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, use_key_in_widget_constructors, unnecessary_string_interpolations
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 
 class MyApp extends StatefulWidget {
   // MyApp({Key? key}) : super(key: key);
@@ -16,6 +15,25 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Widget button(String btnText) {
+    return Expanded(
+      child: RawMaterialButton(
+        onPressed: () {},
+        child: Text(
+          "$btnText",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+        shape: Border.all(color: Colors.black, width: 1),
+        fillColor: Colors.black12,
+        padding: EdgeInsets.all(28),
+        splashColor: Colors.black,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,7 +45,104 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Container(
           child: Column(
-            
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2),
+                    color: Colors.black12,
+                  ),
+                  alignment: Alignment.bottomRight,
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    "OUTPUT",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  button("7"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("8"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("9"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("/"),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  button("4"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("5"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("6"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("x"),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  button("1"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("2"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("3"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("-"),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  button("0"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("c"),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("="),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  button("+"),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+            ],
           ),
         ),
       ),
